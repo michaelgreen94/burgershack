ff5e645b4b435a72e2dd3e265b7347ad2446e68b
Michael Green <michael.green94@hotmail.com>
2018-10-04 09:03:10 -0600

    changes

--- wwwsrc/burgershack/src/views/Home.vue
+++ wwwsrc/burgershack/src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    
-    <burger/>
+    <v-form v-model="valid">
+      <v-text-field v-model="name" :rules="nameRules" :counter="10" label="Name" required></v-text-field>
+      <v-text-field v-model="description" :rules="descriptionRules" label="Description" required></v-text-field>
+      <v-text-field v-model="price" :rules="priceRules" label="Price" required></v-text-field>
+    </v-form>
+    <burger />
   </div>
 </template>
 
@@ -11,6 +15,9 @@ import Burger from "@/components/Burger.vue";
 
 export default {
   name: "home",
+  data: {
+    valid: false
+  },
   mounted() {
     this.$store.dispatch("getAllBurgers");
   },
